dc6f434e7217bbb8e8d32dee42f983f5fc354f8f
Maciej Leszczynski <leszczynski.m@interia.pl>
2020-01-22 19:44:51 +0100

    Style and script changes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Variables
+const display = document.getElementById("screen-line");
+const buttons = document.querySelectorAll("button");
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let result = "";
+let operator = "";
+let num = "";
+let displayedValue = "";
+
+
+// Math operations 
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,6 +25,8 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+
+// Operate function to pick correct operation
 function operate(operator, num1, num2) {
     switch (operator) {
         case "+":
@@ -21,52 +34,62 @@ function operate(operator, num1, num2) {
         case "-":
             return substract(num1, num2);
         case "*":
-            result = multiply(num1, num2);
-            return Number.isInteger(result) ? result : result.toFixed(1);
+            return multiply(num1, num2);
         case "/":
-            result = (num2 === 0) ? "You can't divide by 0" : divide(num1, num2);
-            // console.log(result);
-            if (typeof(result) === String) {
-                console.log(result);
-                return result;
-                // move it to divide function?
+            if (num2 === 0) {
+                return "Don't divide by 0!"
             } else {
-                return Number.isInteger(result) ? result : result.toFixed(1);
+                return divide(num1, num2);
             }
         }
-}
+    }
 
-const display = document.getElementById("screen-line");
-const buttons = document.querySelectorAll("button");
-let result = "";
-let operator = "";
-let num = "";
-Array.from(buttons).forEach(button => {
-    button.addEventListener("click", () => {
-        let values = button.innerHTML;
-        calculate(values);
-        display.innerHTML = result;
-    })
-})
-
+// Button reactions
 function calculate(value) {
-    if (value === "-" || value === "+" || value === "*" || value === "/"){
-        if (num !== "") {
-            num = operate(operator, Number(num), Number(result));
-        } else {
-            num = result;
-        }
-        operator = value;
-        result = '';
-    } else if (value === "=") {
+    if (value === "=") {
         if (num !== "" && operator !== "" && result !== ""){
             result = operate(operator, Number(num), Number(result));
             num = result;
+            displayedValue = result;
+            result = "";
         }
-    }else if (value === "CE") {
+    } else if (value==='+' || value==='-' || value==='*' || value ==='/') {
+        if (num !== "") {
+            num = operate(operator, Number(num), Number(result));
+            displayedValue = "" + String(num) + value;
+        } else {
+            num = result;
+            displayedValue += value;
+        }
+        operator = value;
+        result = '';    
+    } else if (value in numbers) {
+        result += value;
+        displayedValue += value;    
+    } else if (value === "Clear") {
         result = "";
         num = "";
-    } else {
-        result += value;
+        displayedValue = "";
+    } else if (value === ".") {
+        if (!(displayedValue.includes("."))) {
+            result += value;
+            displayedValue += value;
+        }
     }
 }
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener("click", () => {
+        let values = button.innerHTML;
+        console.log(typeof values);
+        console.log(values)
+        calculate(values);
+        display.innerHTML = displayedValue;
+    })
+})
+
+// TO DO:
+// rounding
+// more style
+// bug after mult, div
+// backspace
